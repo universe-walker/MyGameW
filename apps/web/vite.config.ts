1fe6353940f398988cd9da3b71c1731ecb70efae
vseno <email@gmail.com>
2025-08-26 12:23:56 +0300

    Поправил кофиг

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,12 +2,12 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // Update ngrokHost to your current ngrok subdomain if it changes
-const ngrokHost = 'acff64ab3135.ngrok-free.app';
+const ngrokHost = process.env.NGROK_HOST || '9671c34cb909.ngrok-free.app';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 5174,
     host: true, // listen on all interfaces
     allowedHosts: [ngrokHost],
     hmr: {
@@ -17,5 +17,3 @@ export default defineConfig({
     },
   },
 });
-
-
