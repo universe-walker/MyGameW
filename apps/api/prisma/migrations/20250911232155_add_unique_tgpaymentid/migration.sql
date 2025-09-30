1cd7a936899686f199c276c66da25fcfce7103c7
vseno <email@gmail.com>
2025-09-30 12:28:16 +0300

    Правка миграции

--- apps/api/prisma/migrations/20250911232155_add_unique_tgpaymentid/migration.sql
+++ apps/api/prisma/migrations/20250911232155_add_unique_tgpaymentid/migration.sql
@@ -1,2 +1,2 @@
-﻿-- Add unique index on tgPaymentId for strict idempotency
+-- Add unique index on tgPaymentId for strict idempotency
 CREATE UNIQUE INDEX IF NOT EXISTS "BillingPurchase_tgPaymentId_key" ON "BillingPurchase"("tgPaymentId");
